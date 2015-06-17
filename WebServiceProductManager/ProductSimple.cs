649f98cc5bad4d5c91594c3205431526d458b44a
xtastic <kanisso13@gmail.com>
2015-06-17 23:05:29 +0100

    mudanças para ver se o appharbor funciona

---
+++ WebServiceProductManager/ProductSimple.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebServiceProductManager
+{
+    public class ProductSimple
+    {
+        public string ProductCode { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+    }
+}
